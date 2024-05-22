ca3f00240590a1170d8ad88b9daa33af6ae402fe
MJTrejo <mt2904328@gmail.com>
2024-05-22 14:35:57 -0300

    17th commit
    Se ha añadido diseño responsivo a algunas páginas

--- Js/instancias.js
+++ Js/instancias.js
@@ -1,19 +1,19 @@
 /**
- * autor/a:
- * version:
- * fecha: 
+ * Autor/a: Gustavo Salgado
+ * Version: 2
+ * Fecha: 20/05/2024
  * El siguiente script instancias.js y genera a los personajes particulares 
  * en el archivo instancias.js
  * En este archivo estaremos dando las caracteristicas especificas de cada personaje
  */
 //Instanciacion de los objetos , en la inicializacion de cada objeto con valores particulares
-const gorra = new Producto("Gorra","Edicion Limitada ",2.055);
-const funko = new Producto("Funko", "Capitan America", 12.255);
-const mochila = new Producto("Mochila", "Edicion Limitada", 18.199);
-const auto = new Producto("Auto", "Edicion Spiderman", 34.565);
-const moto = new Producto("Moto", "Edicion Ghost Rider", 43.075);
-const camisa = new Producto("Camisa", "Edicion Limitada", 48.095);
-const lego = new Producto("Lego", "Edicion Thor Love And Thunder", 55.555);
-const wakanda = new Producto("Wakanda", "Edicion Especial", 60.655);
-const antman = new Producto("Antman", "Edicion Quantumania", 65.555);
-const hulk = new Producto("Hulk", "Edicion Hulk", 75.555);
+const gorra = new Producto("Gorra","Edición limitada ",2.055);
+const funko = new Producto("Funko", "Capitán América", 12.255);
+const mochila = new Producto("Mochila", "Edición limitada", 18.199);
+const auto = new Producto("Auto", "Spiderman", 34.565);
+const moto = new Producto("Moto", "Ghost Rider", 43.075);
+const camisa = new Producto("Camisa", "Edición limitada", 48.095);
+const lego = new Producto("Lego", "Love and Thunder", 55.555);
+const wakanda = new Producto("Wakanda", "Edición especial", 60.655);
+const antman = new Producto("Antman", "Quantumania", 65.555);
+const hulk = new Producto("Hulk", "Edición especial", 75.555);
